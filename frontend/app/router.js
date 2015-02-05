aa99fb7b918403c165aa7c4fc725d4702eb09aba
juanesarango <juanes.ao@gmail.com>
2015-02-04 20:43:41 -0500

    change routers

--- frontend/app/router.js
+++ frontend/app/router.js
@@ -6,20 +6,25 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.resource("studies", function() {
+    this.resource("studies", function() {
     this.route("new");
   });
-
   this.resource("study", { path: '/study/:study_id' }, function() {
-    this.resource("variables", function() {
-      this.route("new");
-    });
-    this.resource("variable", { path: '/variable/:variable_id' }, function() {});
+    this.route("edit");
+  });
 
-    this.resource("policies", function() {
-      this.route("new");
-    });
-    this.resource("policy", { path: '/policy/:policy_id' }, function() {});
+  this.resource("variables", function() {
+    this.route("new");
+  });
+  this.resource("variable", { path: '/variable/:variable_id' }, function() {
+    this.route("edit");
+  });
+  
+  this.resource("policies", function() {
+    this.route("new");
+  });
+  this.resource("policy", { path: '/policy/:policy_id' }, function() {
+    this.route("edit");
   });
 });
 
