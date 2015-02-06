3a4d93123725c787304ec35f12095f30cbafe37d
Julian <julian.kmazo@gmail.com>
2015-02-05 22:17:35 -0500

    Transitions. Please install npm again

---
+++ frontend/app/transitions.js
@@ -0,0 +1,45 @@
+export default function(){
+	this.transition(
+		this.fromRoute('objectives.index'),
+		this.toRoute('objectives.new'),
+		this.use('toLeft'),
+		this.reverse('toRight')
+	);
+
+	this.transition(
+		this.toRoute('policy'),
+		this.use('toLeft'),
+		this.reverse('toRight')
+	);
+
+	this.transition(
+		this.toRoute('study'),
+		this.use('toLeft'),
+		this.reverse('toRight')
+	);
+
+	this.transition(
+		this.toRoute('objectives'),
+		this.use('toLeft'),
+		this.reverse('toRight')
+	);
+	this.transition(
+		this.toRoute('new'),
+		this.use('toLeft'),
+		this.reverse('toRight')
+	);
+
+	this.transition(
+		this.fromRoute('studies.index'),
+		this.toRoute('studies.new'),
+		this.use('toUp'),
+		this.reverse('toDown')
+	);
+
+	this.transition(
+		this.fromRoute('policies.index'),
+		this.toRoute('policies.new'),
+		this.use('toUp'),
+		this.reverse('toDown')
+	);
+}
