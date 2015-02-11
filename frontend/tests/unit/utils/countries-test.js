0f646fcecfc795f9fcb418dba6aded0fbca2be8a
juanesarango <juanes.ao@gmail.com>
2015-02-11 17:46:20 -0500

    several computed properties to pyp model

---
+++ frontend/tests/unit/utils/countries-test.js
@@ -0,0 +1,9 @@
+import countries from 'frontend/utils/countries';
+
+module('countries');
+
+// Replace this with your real tests.
+test('it works', function() {
+  var result = countries();
+  ok(result);
+});
