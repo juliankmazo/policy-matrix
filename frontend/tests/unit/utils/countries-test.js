8fb85b3e78ff330d5e00c369194f68ae9c0b7e8d
Julian <julian.kmazo@gmail.com>
2015-02-20 13:24:07 -0500

    Delet all the fucking tests

--- frontend/tests/unit/utils/countries-test.js
+++
@@ -1,9 +0,0 @@
-import countries from 'frontend/utils/countries';
-
-module('countries');
-
-// Replace this with your real tests.
-test('it works', function() {
-  var result = countries();
-  ok(result);
-});
